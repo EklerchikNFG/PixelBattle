e517f0e05d6e923c7434bc360d54226f80762992
maytiha <aaanasyasia.fi@gmail.com>
2026-05-26 18:33:01 +0300

    заполнили код класс для мира

--- serverPB/World.cs
+++ serverPB/World.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace serverPB
 {
-    internal class World
+    public class World
     {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public byte[,] Pixels { get; set; }
+        public List<string> Players { get; set; } = new List<string>();
+        public Dictionary<string, DateTime> LastPixelTime { get; set; }=new();
+        public Dictionary<string, int> PlayerScores { get; set; }=new();
+        public World(int id, string name, int width, int height)
+        {
+            ID = id; Name = name;
+            Width = width; Height = height;
+            Pixels= new byte[width, height];
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Pixels[x, y] = 0;
+                }
+            }
+
+
+        }
+
     }
 }
